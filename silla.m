d527a7f22ab2b07c3f06f19895ce32ea768afb00
Hugo <hugo1798@hotmail.com>
2023-05-01 21:31:53 -0600

    Se realizan modificaciones pedidas en ultima junta, codigo no corre de manera adecuada, revisar

--- silla.m
+++ silla.m
@@ -12,107 +12,173 @@ FFT4 = file(:,4);
 FFT5 = file(:,5);
 FFT6 = file(:,6);
 FFT7 = file(:,7);
+target = file(:,8);
 
-%Aumentamos en un valor de 3 para poder crear una ventana que pueda
-    %analizar todos los datos del archivo. En caso de que se quiera cambiar
-    %el valor de la ventana, se tiene que cambiar el numero al que se suma
-    %la variable de iteracion n
-for n = 2:3:length(file)
+[r p] = size(file);
+
+suma1 = 0;
+suma = 0;
+
+%Parametros de ajuste de los pesos
+alfa1 = 1;
+alfa2 = 0;
+alfa3 = 0;
+
+%% Estructura de la red
+
+N1 = 21; %Capa de entrada
+N2 = 52; %Primer capa oculta
+N3 = 24; %Segunda capa oculta
+N4 = 1;  %Capa de salida
+
+%Pesos
+%W1 tiene el tamano de N1 multiplicado por N2
+W1 = randn(N1, N2);
+%W2 tiene el tamano de N2 multiplicado por N3
+W2 = randn(N2, N3);
+%W3 tiene el tamano de N3 multiplicado por N4
+W3 = randn(N3, N4);
+
+m4 = 3;
+
+Se = [];
+
+Se(1) = 0;
+
+grad1 = zeros(N1, 1);
+grad2 = zeros(N2, 1);
+grad3 = zeros(N3, 1);
+
+% Parametros de la funcion sigmoide, tienen que ser el numero de entradas
+
+% Para la funcion sigmoide 1
+a1 = ones(N1, 1);
+b1 = ones(N1, 1);
+c1 = ones(N1, 1);
+d1 = ones(N1, 1);
+
+% Para la funcion sigmoide 2
+a2 = ones(N2, 1);
+b2 = ones(N2, 1);
+c2 = ones(N2, 1);
+d2 = ones(N2, 1);
+
+% Para la funcion sigmoide 3
+a3 = ones(N3, 1);
+b3 = ones(N3, 1);
+c3 = ones(N3, 1);
+d3 = ones(N3, 1);
+
+%Dentro de este ciclo for se realiza el movimiento de la ventana para ir
+%asignando los valores a cada una de las entradas, de manera que podamos
+%recorrernos a traves de todo el documento de los datos adquiridos
+for n = 2:1:24
     
-    if (n == 146)
+    if (n == 24)
         break
     end
 
     %Asignamos los valores de las transformadas de fourier a nuestras
     %entradas
-    u1 = [FFT1(n,1),FFT2(n,1),FFT3(n,1),FFT4(n,1),FFT5(n,1),FFT6(n,1),FFT7(n,1)]
-    u2 = [FFT1(n + 1,1),FFT2(n + 1,1),FFT3(n + 1,1),FFT4(n + 1,1),FFT5(n + 1,1),FFT6(n + 1,1),FFT7(n + 1,1)]
-    u3 = [FFT1(n + 2,1),FFT2(n + 2,1),FFT3(n + 2,1),FFT4(n + 2,1),FFT5(n + 2,1),FFT6(n + 2,1),FFT7(n + 2,1)]
+    u1 = [FFT1(n,1),FFT2(n,1),FFT3(n,1),FFT4(n,1),FFT5(n,1),FFT6(n,1),FFT7(n,1)];
+    u2 = [FFT1(n + 1,1),FFT2(n + 1,1),FFT3(n + 1,1),FFT4(n + 1,1),FFT5(n + 1,1),FFT6(n + 1,1),FFT7(n + 1,1)];
+    u3 = [FFT1(n + 2,1),FFT2(n + 2,1),FFT3(n + 2,1),FFT4(n + 2,1),FFT5(n + 2,1),FFT6(n + 2,1),FFT7(n + 2,1)];
     
-    suma1 = 0;
-    suma = 0;
+    %Vector para la primera capa de tamano 21
+    uA = [u1 u2 u3];
     
-    entradas = [u1 u2 u3];
+    %Funcion de activavion exponencial de forma a / (1 + exp (-c * (x - b))) + d
+    %Primera capa
+    for i1 = 1:N1
     
-    alfa1 = 1;
-    alfa2 = 0;
-    alfa3 = 0;
+        Sig1(i1) = (a1(i1) / (1 + exp(-c1(i1) * (uA(i1) - b1(i1))))) + d1(i1);   
+        
+        dSig1(i1)= (a1(i1) / (1 + exp(-c1(i1) * (uA(i1) - b1(i1))))) * ((c1(i1)*exp(-c1(i1) * (uA(i1) - b1(i1)) )) / (1 + exp(-c1(i1) * (uA(i1) - b1(i1)) )) );   
     
-    %tiene que coincidir con la clasificacion o etiqueta proporcionada
-    y1ast = 1;
-    W1 = [1 1 1 1 1 1 1];
-    W2 = 0;
-    W3 = 0;
-    
-    %determinado por las entradas 
-    v1 = [3 3 3];
-    %determinado por las funciones de activacion
-    v2 = [3 3 3 3 3 3 3];
-    v3 = [3 3 3 3 3];
-    v4 = [3 3 3 3];
-    
-    %vectores auxiliares para tener mismo tamaño de
-    %los vectores
-    vaux1 = [1 1 1 1 1 1 1];
-    vaux2 = [1 1 1 1 1 1 1];
-    vaux3 = [1 1 1 1 1 1 1];
-    vaux4 = [1 1 1 1 1 1 1];
-    
-    %almacenando vector en vector auxiliar
-    for i = 1:length(v1)
-        vaux1(i) = v1(i); 
     end
+
+    %Vector para la segunda capa de tamano 52
+    %Se realiza una transposicion de los datos de W1 para poder multiplicar y
+    %que nos quede solamente la N de la siguiente capa
+    uB = W1' .* Sig1;
+
+    %Segunda capa
+    for i2 = 1:N2
     
-    for i = 1:length(v2)
-        vaux2(i) = v2(i); 
+        Sig2(i2) = (a2(i2) / (1 + exp(-c2(i2) * (uB(i2) - b2(i2))))) + d2(i2);   
+        
+        dSig2(i2)= (a2(i2) / (1 + exp(-c2(i2) * (uB(i2) - b2(i2))))) * ((c2(i2)*exp(-c2(i2) * (uB(i2) - b2(i2)) )) / (1 + exp(-c2(i2) * (uB(i2) - b2(i2)) )) );   
+    
     end
+
+    %Vector para la tercera capa de tamano 24
+    uC = W2' .* Sig2;
+
+    %Tercera capa
+    for i3 = 1:N3
     
-    for i = 1:length(v3)
-        vaux3(i) = v3(i); 
+        Sig3(i3) = (a3(i3) / (1 + exp(-c3(i3) * (uC(i3) - b3(i3))))) + d3(i3);   
+        
+        dSig3(i3)= (a3(i3) / (1 + exp(-c3(i3) * (uB(i3) - b3(i3))))) * ((c3(i3)*exp(-c3(i3) * (uC(i3) - b3(i3)) )) / (1 + exp(-c3(i3) * (uC(i3) - b3(i3)) )) );   
+    
     end
-    
-    for i = 1:length(v4)
-        vaux4(i) = v4(i); 
-    end
-    
-    %% backpropagation
-    
-    % Para calcular sumatoria para W1
-    for n1 = 1:length(entradas)
-        sumaW1 = (y1ast - vaux2.*(W1 .* vaux1.*(entradas(n1)))) .* (vaux2.*(W1 .* vaux1.*(entradas(n1)))) .* (vaux1.*(entradas(n1)))
-        suma = suma + sumaW1;
-    end
-    % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
-    
-    % Para calcular el peso
-    W1 = W1 - (alfa1 * (-2) .* suma);
-    %Regresa un solo vector del tamanio del vector auxiliar
-    
-    % Para calcular sumatoria para W2
-    for n2 = 1:length(entradas)
-        sumaW2 = (y1ast - vaux3 .* (W2 .* vaux2.*(W1 .* vaux1(entradas(n2))))) .* ...
-            (vaux3 .*(W2 .* vaux2 .* (W1 .* vaux1(entradas(n2))))) .* ...
-            (vaux2 .* (W1 .* vaux1 .*(entradas(n2))))
-        suma2 = suma2 + sumaW2;
-    end
-    % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
-    
-    % Para calcular el peso
-    W2 = W2 - (alfa2 * (-2) .* suma2);
-    %Regresa un solo vector del tamanio del vector auxiliar
-    
-    % Para calcular sumatoria para W3
-    for n3 = 1:length(entradas)
-        sumaW3 = (y1ast - vaux4 .* (W3 .* vaux3.*(W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3))))) .* ...
-            (vaux4 .*(W3 .* vaux3 .* (W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3)))))) .* ...
-            (vaux3 .* (W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3))))))
-        suma3 = suma3 + sumaW3;
-    end
-    % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
-    
-    % Para calcular el peso
-    W3 = W3 - (alfa3 * (-2) .* suma3);
+
+    %Cuarta capa
+    Mov_NN = m4 * (W3' .* Sig3);
+
 end
+    %% backpropagation
+for n2 = 1 : 1 : 21
+
+    e(1,n2) = target(n2) - Mov_NN(n2);
+
+%     Se(1,n2) = Se(n2) + 0.5 * e(n2)^2;
+
+    grad1(n2) = -2 * (grad1(n2) + (e(n2) * Mov_NN(n2) * Sig3(n2) * dSig2(n2) * Sig1(n2)));
+    grad2(n2) = -2 * (grad2(n2) + (e(n2) * Mov_NN(n2) * dSig3(n2) * Sig2(n2)));
+    grad3(n2) = -2 * (grad3(n2) + (e(n2) * m4 * Sig3(n2)));
+
+    W3 = W3 - alfa3 * grad3;
+    W2 = W2 - alfa2 * grad2;
+    W1 = W1 - alfa1 * grad1;
+end
+
+%     % Para calcular sumatoria para W1
+%     for n1 = 1:length(entradas)
+%         sumaW1 = (y1ast - vaux2.*(W1 .* vaux1.*(entradas(n1)))) .* (vaux2.*(W1 .* vaux1.*(entradas(n1)))) .* (vaux1.*(entradas(n1)))
+%         suma = suma + sumaW1;
+%     end
+%     % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
+%     
+%     % Para calcular el peso
+%     W1 = W1 - (alfa1 * (-2) .* suma);
+%     %Regresa un solo vector del tamanio del vector auxiliar
+%     
+%     % Para calcular sumatoria para W2
+%     for n2 = 1:length(entradas)
+%         sumaW2 = (y1ast - vaux3 .* (W2 .* vaux2.*(W1 .* vaux1(entradas(n2))))) .* ...
+%             (vaux3 .*(W2 .* vaux2 .* (W1 .* vaux1(entradas(n2))))) .* ...
+%             (vaux2 .* (W1 .* vaux1 .*(entradas(n2))))
+%         suma2 = suma2 + sumaW2;
+%     end
+%     % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
+%     
+%     % Para calcular el peso
+%     W2 = W2 - (alfa2 * (-2) .* suma2);
+%     %Regresa un solo vector del tamanio del vector auxiliar
+%     
+%     % Para calcular sumatoria para W3
+%     for n3 = 1:length(entradas)
+%         sumaW3 = (y1ast - vaux4 .* (W3 .* vaux3.*(W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3))))) .* ...
+%             (vaux4 .*(W3 .* vaux3 .* (W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3)))))) .* ...
+%             (vaux3 .* (W2 .* vaux2 .* (W1 .* vaux1 .* (entradas(n3))))))
+%         suma3 = suma3 + sumaW3;
+%     end
+%     % Regresa la suma de 3 vectores auxiliares que se dan por las entradas u
+%     
+%     % Para calcular el peso
+%     W3 = W3 - (alfa3 * (-2) .* suma3);
+
 %Regresa un solo vector del tamanio del vector auxiliar
 
 % W1 = W1 - (alfa1 * (-2) 
